559e23f23bca8af9d9bf51cf56e6482ac7540912
Ahmed Morad <ahmed.morad@gmail.com>
2015-09-29 20:58:26 -0400

    Added Perl script

---
+++ scripts/update_paths.pl
@@ -0,0 +1,21 @@
+$num_args = $#ARGV + 1;
+if ($num_args != 2) {
+    print "\nUsage: update_paths.pl dev|prod filename\n";
+    exit;
+}
+
+print "\nThis script switches between Dev and Prod file paths in a facetview2 browser HTML\n";
+
+$environment=$ARGV[0];
+$filename=$ARGV[1];
+
+if ($environment eq 'dev') {
+  $cmd1 = "perl -pi -e 's/vivo\\/themes\\/dco\\/js\\/facetview2\\//facetview2\\//g' ".$filename;
+  $cmd2 = "perl -pi -e 's/vivo\\/themes\\/dco\\/css\\/browsers.css/browsers.css/g' ".$filename;
+} elsif ($environment eq 'prod') {
+  $cmd1 = "perl -pi -e 's/facetview2\\//vivo\\/themes\\/dco\\/js\\/facetview2\\//g' ".$filename;
+  $cmd2 = "perl -pi -e 's/browsers.css/vivo\\/themes\\/dco\\/css\\/browsers.css/g' ".$filename;
+}
+
+system($cmd1);
+system($cmd2);
