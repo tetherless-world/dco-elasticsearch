d6602a7c4f807bad3fd619299533c4b4e1a2c8f3
mrpatrickwest <westp@rpi.edu>
2015-10-06 17:51:34 -0400

    Cron scripts for ingesting data into elasticsearch
    Defaults to 4 threads per script, offset by 15 minutes in the cron.

---
+++ scripts/es-cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd /opt/backups/es
+find . -atime +5 -delete
+
---
+++ scripts/es-people-ingest.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ofile=`date +"%Y%m%d-%H-%M-%S".people.bulk`
+
+cd /opt/dco/dco-elasticsearch/ingest
+
+/usr/bin/python3 /opt/dco/dco-elasticsearch/ingest/ingest-people.py --threads 4 --sparql http://localhost:2020/vivo/query --es http://localhost:49200 --publish ${ofile} >> /var/log/publication-ingest.log
+
+gzip ${ofile}
+mv ${ofile}.gz /opt/backups/es
+
