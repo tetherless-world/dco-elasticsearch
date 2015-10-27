7f868c788bb3caaf19bddd5f9e0fecf24ddc4a0e
mrpatrickwest <westp@rpi.edu>
2015-10-27 18:39:43 -0400

    Added mapping options as not defaulted in script
    In the other scripts the mapping file defaults to the specific .json
    file under mappings directory. With this datasets ingest there is no
    default so have to specify it on the command line.

--- scripts/es-datasets-ingest.sh
+++ scripts/es-datasets-ingest.sh
@@ -4,8 +4,7 @@ ofile=`date +"%Y%m%d-%H-%M-%S".dataset.bulk`
 
 cd /opt/dco/dco-elasticsearch/ingest 
 
-/usr/bin/python3 /opt/dco/dco-elasticsearch/ingest/ingest-datasets.py --threads 4 --sparql http://localhost:2020/vivo/query --es
-http://localhost:49200 --publish $ofile >> /var/log/dataset-ingest.log
+/usr/bin/python3 /opt/dco/dco-elasticsearch/ingest/ingest-datasets.py --threads 4 --mapping mappings/dataset.json --sparql http://localhost:2020/vivo/query --es http://localhost:49200 --publish $ofile >> /var/log/dataset-ingest.log
 
 gzip $ofile
 
