57d19b5e0b08b7a76ea9d0e36155c97662a2754c
Stephan Zednik <lokipatera@gmail.com>
2015-10-27 14:44:32 -0600

    fixed ingest log filename

--- scripts/es-datatypes-ingest.sh
+++ scripts/es-datatypes-ingest.sh
@@ -4,7 +4,7 @@ ofile=`date +"%Y%m%d-%H-%M-%S".datatypes.bulk`
 
 cd /opt/dco/dco-elasticsearch/ingest
 
-/usr/bin/python3 /opt/dco/dco-elasticsearch/ingest/ingest-datatypes.py --threads 4 --sparql http://localhost:2020/vivo/query --es http://localhost:49200 --publish ${ofile} >> /var/log/publication-ingest.log
+/usr/bin/python3 /opt/dco/dco-elasticsearch/ingest/ingest-datatypes.py --threads 4 --sparql http://localhost:2020/vivo/query --es http://localhost:49200 --publish ${ofile} >> /var/log/datatypes-ingest.log
 
 gzip ${ofile}
 mv ${ofile}.gz /opt/backups/es
