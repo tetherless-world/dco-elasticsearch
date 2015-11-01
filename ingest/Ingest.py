bcbf9c09ddd14d124945e6a3ba3846fa2a9d7598
zhonghao1014 <zhonghao1014@gmail.com>
2015-11-01 16:55:56 -0500

    Documentation in Ingest.py updated; please check if this way works.

--- ingest/Ingest.py
+++ ingest/Ingest.py
@@ -7,10 +7,20 @@ import json
 import requests
 
 class Ingest:
-    """say something"""
+    """Helper class governing an ingest process."""
 
     def __init__(self, object_index, object_type,
                  get_objects_query_location, describe_object_query_location, variable_name_sparql):
+        """
+        Constructor method of the Ingest class.
+        :param object_index:                    object index
+        :param object_type:                     object type
+        :param get_objects_query_location:      the location of the .rq file to list all the objects
+        :param describe_object_query_location:  the location of the .rq file to describe all the objects
+        :param variable_name_sparql:            the variable name representing the object in the .rq files,
+                                                    e.g. "?dataset"
+        :return:                                an instance of Ingest.
+        """
         self.object_index = object_index
         self.object_type = object_type
         self.records = []
@@ -20,28 +30,38 @@ class Ingest:
 
 
     def load_file(filepath):
+        """
+        Helper function mainly used by the __init__ to load the .rq files.
+        :param filepath:    file path
+        :return:            file content in string format
+        """
         with open(filepath) as _file:
             return _file.read().replace('\n', " ")
 
-    # get_metadata: member function to be used in process_object
+
     def get_metadata(index, type, id):
+        """
+        Helper function to create the JSON string of the metadata of an object.
+        :param index:   object index
+        :param type:    object type
+        :param id:      unique identifier of the object
+        :return:
+            a JSON string representing the metadata information of the object,
+                            e.g. {"index": {"_id": "http://...", "_type": "dataset", "_index": "dco"}}
+        """
         return {"index": {"_index": index, "_type": type, "_id": id}}
 
-    def select(endpoint, query):
-        sparql = SPARQLWrapper(endpoint)
-        sparql.setQuery(query)
-        sparql.setReturnFormat(JSON)
-        results = sparql.query().convert()
-        return results["results"]["bindings"]
 
-    def get_objects(endpoint, get_objects_query, object_type):
-        r = Ingest.select(endpoint, get_objects_query)
-        return [rs[object_type]["value"] for rs in r]
-
-    # process_object: used by generate
     def process_object(self, object, endpoint, create_object_doc_function):
-        # ds = create_dataset_doc(dataset=dataset, endpoint=endpoint) ==>
-        # ds = create_object_doc_function(dataset=object, endpoint=endpoint)
+        """
+        Helper function used by generate() to process each object and generate the attribute
+        :param object:      the object to be described
+        :param endpoint:    SPARQL endpoint
+        :param create_object_doc_function:
+            Externally defined, case-varying function to create the JSON document. The function name is passed in
+            originally through Ingest.generate(...).
+        :return:            An object entry in JSON
+        """
         ds = create_object_doc_function(dataset=object, endpoint=endpoint,
                                         describe_object_query=self.describe_object_query,
                                         variable_name_sparql=self.variable_name_sparql)
@@ -51,21 +71,61 @@ class Ingest:
             return []
 
 
+    def select(endpoint, query):
+        """
+        Helper function used by get_objects
+        :param endpoint:    SPARQL endpoint
+        :param query:       the SPARQL query to get the list of objects
+        :return:
+            a list of objects with its type and uri values, e.g.
+                [{'dataset': {'value': 'http://...', 'type': 'uri'}}, ...]
+        """
+        sparql = SPARQLWrapper(endpoint)
+        sparql.setQuery(query)
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+        return results["results"]["bindings"]
+
+
+    def get_objects(endpoint, get_objects_query, object_type):
+        """
+        Helper function used by Ingest.generate(...).
+        :param endpoint:            SPARQL endpoint
+        :param get_objects_query:   the SPARQL query to get the list of objects
+        :param object_type:         object type
+        :return:
+            a list of all the objects' uri values
+        """
+        r = Ingest.select(endpoint, get_objects_query)
+        return [rs[object_type]["value"] for rs in r]
+
+
     def generate(self, threads, sparql, create_object_doc_function):
+        """
+        The major method to let an instance of Ingest generate the JSON records.
+        :param threads:
+        :param sparql: SPARQL endpoint
+        :param create_object_doc_function:
+        :return:
+            the output JSON records of this Ingest process.
+        """
         pool = multiprocessing.Pool(threads)
-        # params = [(dataset, sparql) for dataset in get_datasets(endpoint=sparql)] ==>
         params = [(object, sparql, create_object_doc_function)
                   for object in Ingest.get_objects(endpoint=sparql,
                                             get_objects_query=self.get_objects_query,
                                             object_type=self.object_type)]
-        # return list(itertools.chain.from_iterable(pool.starmap(process_object_function, params)))
-        # self.records = list(itertools.chain.from_iterable(pool.starmap(process_object_function, params)))
         self.records = list(itertools.chain.from_iterable(pool.starmap(self.process_object, params)))
-
         return self.records
 
 
     def publish(self, bulk, endpoint, rebuild, mapping):
+        """
+        The majar method to publish the result of the Ingest process.
+        :param bulk:        the bulk file containing the ingest result
+        :param endpoint:    SPARQL endpoint
+        :param rebuild:
+        :param mapping:     the mapping file
+        """
         # if configured to rebuild_index
         # Delete and then re-create to publication index (via PUT request)
 
